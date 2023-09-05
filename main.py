8cfd8013d961949fdf31378e767c0822a22b1557
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-09-05 12:31:10 +0300

    project in progress

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ def main ():
     bookList = []
     
     
+    choice = 0
+    while choice !=4:
+        print("***Books library***")
+        print("1) Add a book")
+        print("2) Search for a book")
+        print("3) Display books")
+        print ("4) Delete a book")
+        choice = int(input())
     
+if __name__ == "__main__":
+    main()    
     
